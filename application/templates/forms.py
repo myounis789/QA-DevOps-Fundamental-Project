797774ac6ce91cacb2c96897290a09cef8b668b5
myounis789 <mohammedyounis789@gmail.com>
2021-11-11 15:43:40 +0000

    moved forms.py into application folder

--- application/templates/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, SelectField, SubmitField, DateField
-
-class AddUser(FlaskForm):
-    custName = StringField("name")
-    email = EmailField("mail")
-    birthDate = DateField("dob")
-    number = IntegerField("num")
