729f50eb92fe932d46737ba0154682eae5cecdb1
myounis789 <mohammedyounis789@gmail.com>
2021-11-11 03:00:39 +0000

    TBS-76 TBS-77 Added a users database model

--- application/models.py
+++ application/models.py
@@ -1,1 +1,11 @@
 from application import db
+
+class Users(db.Model):
+    UserId = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.CHAR(20))
+    UserType = db.Column(db.CHAR(10))
+    Email = db.Column(db.String)
+    DoB = db.Column(db.Date)
+    Phone = db.Column(db.Integer(15))
+    LoginId = db.Column(db.String(10))
+    
