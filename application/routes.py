5a43c33d698d9b72e64729ab21d93defcbed3958
myounis789 <mohammedyounis789@gmail.com>
2021-11-12 16:09:06 +0000

    TBS-38 #IN-PROGRESS Added a generateKey() function to generate random loginID

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, redirect
 from application import app, db
 from application.forms import AddUser, CustomerLogin
+from application.models import Users
+# ----------------------------------------------
+import string, random
+
+def generateKey(size=8, chars=string.ascii_letters + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+# ----------------------------------------------
 
 @app.route("/")
 def homePage():
@@ -18,17 +25,17 @@ def customerLogin():
 
 @app.route("/saveCust", methods=["GET","POST"])
 def saveCust():
-    form = CustomerLogin()
+    form = AddUser()
     if request.method == 'POST':
         name = form.custName.data
         userType = 'customer'
         email = form.email.data
         dob = form.birthDate.data
         phone = form.number.data
-        loginid = "DKCWeb12"
+        loginid = generateKey()
 
-        newCust = Users (Name = Name, UserType= = userType, Email = email, DoB = dob ,Phone = phone, LoginId=loginid)
+        newCust = Users(Name = name, UserType = userType, Email = email, DoB = dob ,Phone = phone, LoginId=loginid)
         db.session.add(newCust)
         db.session.commit()
-        return redirect("/")
-    return render_template("displayId.html", customer=form)
+        return render_template("displayId.html", customer=newCust)
+    return render_template("registration.html")
