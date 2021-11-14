64def3fe205d0fc5fd305e56eed0feefdc681eda
myounis789 <mohammedyounis789@gmail.com>
2021-11-14 00:06:05 +0000

    TBS-14  #IN-PROGRESS Added bookings table and established relationship

--- application/models.py
+++ application/models.py
@@ -1,10 +1,21 @@
 from application import db
 
 class Users(db.Model):
-    UserId = db.Column(db.Integer, primary_key=True)
+    UserId = db.Column(db.Integer, primary_key=True, nullable=False)
     Name = db.Column(db.CHAR(20))
     UserType = db.Column(db.CHAR(10))
     Email = db.Column(db.String(50), unique=True)
     DoB = db.Column(db.Date)
     Phone = db.Column(db.String(15), unique=True)
     LoginId = db.Column(db.String(10), unique=True)
+    Bookings = db.relationship("bookings", backref="user")
+
+class Bookings(db.Model):
+    bookingId = db.Column(db.Integer, primary_key=True, nullable=False)
+    description = db.Column(db.String(50))
+    specialRequest = db.Column(db.String(50))
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
+    status = db.column(db.String(15))
+    uid = db.column(db.Integer, db.ForeignKey(Users.UserId))
+    
