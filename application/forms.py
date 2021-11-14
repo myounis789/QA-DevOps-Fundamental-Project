ff0bc099c8c7ff19a7059d84f7fd4f66c300b0f7
myounis789 <mohammedyounis789@gmail.com>
2021-11-14 16:19:04 +0000

    TBS-57 #IN-PROGRESS Designed flask form for booking system

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, SelectField, SubmitField, DateField
+from wtforms import IntegerField, StringField, SelectField, SubmitField, SelectMultipleField, widgets
+from wtforms import DateField, TimeField
+from datetime import datetime
+# ---------------------------------------------------------------
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+# ---------------------------------------------------------------
 
 class AddUser(FlaskForm):
     custName = StringField("Full Name")
@@ -17,3 +26,12 @@ class AdminLogin(FlaskForm):
     adminName = StringField("Full Name")
     adminId = StringField("Login-ID")
     submit = SubmitField("Log in")
+
+class AddBooking(FlaskForm):
+    description = StringField("Description")
+    date = DateField("Booking Date")
+    time = TimeField("Time")
+    adults = IntegerField("No. of Adults")
+    children = IntegerField("No. of Children")
+    requests = MultiCheckboxField("Special Requests", choices=[('Power Socket'),('Window Seat'),('Wheelchair Access'),('Baby Changing facility'), ('Prayer Room'), ('Private Family room')] )
+    submit = SubmitField("Book Now!")
